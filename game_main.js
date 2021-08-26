dc21dbe17ed12b37f625bad5ef1e4e8108b1d491
saim514 <81713694+saim514@users.noreply.github.com>
2021-08-26 16:01:22 -0400

    Add files via upload

--- game_main.js
+++ game_main.js
@@ -1,8 +1,8 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
+var player1_name = localStorage.getItem("player1_name");
+var player2_name = localStorage.getItem("player2_name");
 
-	player1_score = 0;
-	player2_score = 0;
+var	player1_score = 0;
+var	player2_score = 0;
 
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
